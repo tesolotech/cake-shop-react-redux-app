91886fa03d82dfeca6da0d3f74b4819df7e38eb0
megasoft909@gmail.com <megasoft909@gmail.com>
2020-07-30 01:51:03 +0530

    Un comment out module

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
     <Provider store={store}>
       <div className="App">
         <UserContainer />
-        {/* <IceCreamContainer />
+        <IceCreamContainer />
         <HooksCakeContainer />
-        <CakeContainer /> */}
+        <CakeContainer />
       </div>
     </Provider>
   );
